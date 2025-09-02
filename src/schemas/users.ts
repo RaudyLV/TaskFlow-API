2539027a8d63c277a8daee1ffe8157c9f6052166
Raudy <Raudy206@gmail.com>
2025-09-01 21:51:56 -0400

    fix(users): hace el nombre opcional para el esquema de actualizar

--- src/schemas/users.ts
+++ src/schemas/users.ts
@@ -15,7 +15,7 @@ export const registerUserSchema = z.object({
 });
 
 export const updateUserSchema = z.object({
-    name: z.string().min(2, 'Name must be at least 2 characters'),
+    name: z.string().min(2, 'Name must be at least 2 characters').optional(),
     role: z.enum(['user', 'admin'], 'Role must be admin or user').optional()
 });
 
