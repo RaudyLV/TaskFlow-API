dfe51db05f96eb25d72b9573ea0969c217905ef6
Raudy <Raudy206@gmail.com>
2025-09-02 10:33:36 -0400

    feat: agrega middlewares de autenticacion a las rutas

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema";
 import { createTaskSchema, updateTaskSchema } from "../schemas/tasks";
 import { getAllTasksController, createTaskController, deleteTaskController, updateTaskController } from "../controllers/tasks";
+import { isAdmin } from "../middlewares/isAdmin";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 
 export default (router: Router) => {
-    router.get('/tasks/', getAllTasksController);
-    router.post('/tasks/', validateSchema(createTaskSchema), createTaskController);
-    router.delete('/tasks/:id', deleteTaskController);
-    router.patch('/tasks/:id', validateSchema(updateTaskSchema), updateTaskController);
+    router.get('/tasks/', isAuthenticated, getAllTasksController);
+    router.post('/tasks/', isAuthenticated, validateSchema(createTaskSchema), createTaskController);
+    router.delete('/tasks/:id', isAuthenticated, isAdmin, deleteTaskController);
+    router.patch('/tasks/:id', isAuthenticated, isAdmin, validateSchema(updateTaskSchema), updateTaskController);
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,10 +2,13 @@ import {Router} from 'express';
 import { getAllUsersController, getUserByIdController, updateUserController, deleteUserController } from '../controllers/users';
 import { validateSchema } from '../middlewares/validateSchema';
 import { updateUserSchema } from '../schemas/users';
+import { isAdmin } from "../middlewares/isAdmin";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
+import { isOwner } from '../middlewares/isOwner';
 
 export default (router: Router) =>{
-    router.get('/users/:id', getUserByIdController);
-    router.get('/users', getAllUsersController);
-    router.patch('/users/:id', validateSchema(updateUserSchema), updateUserController);
-    router.delete('/users/:id', deleteUserController);
+    router.get('/users/:id', isAuthenticated, isAdmin, getUserByIdController);
+    router.get('/users', isAuthenticated, isAdmin, getAllUsersController);
+    router.patch('/users/:id', isAuthenticated, isOwner, validateSchema(updateUserSchema), updateUserController);
+    router.delete('/users/:id', isAuthenticated, isOwner, deleteUserController);
 }
